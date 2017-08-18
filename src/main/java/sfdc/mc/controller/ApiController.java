04e5095dedb5bfc5d881a290cfdde83d752375ff
matrostik <matrostik@gmail.com>
2017-08-18 21:58:56 +0300

    api controller

---
+++ src/main/java/sfdc/mc/controller/ApiController.java
@@ -0,0 +1,27 @@
+package sfdc.mc.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static java.lang.System.out;
+
+/**
+ * API Controller
+ */
+@RestController
+@RequestMapping("api")
+public class ApiController {
+
+    @RequestMapping("/test")
+    public ResponseEntity test(@RequestParam(value="name", defaultValue="World") String name) {
+        return new ResponseEntity(String.format("Hello, %s!", name), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/post", method = RequestMethod.POST, headers = "Accept=application/json")
+    public ResponseEntity post(@RequestBody String json) {
+        out.println("************** " + json + " *****************");
+        return new ResponseEntity("OK",HttpStatus.OK);
+    }
+
+}
