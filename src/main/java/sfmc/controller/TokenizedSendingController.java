5daaa53cb5c0235f630e9921a61f5e3477e5a174
Ros Haitovich <matrostik@gmail.com>
2018-01-09 09:42:31 +0200

    tokenizer beta

--- src/main/java/sfmc/controller/TokenizedSendingController.java
+++ src/main/java/sfmc/controller/TokenizedSendingController.java
@@ -6,10 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import sfmc.model.TokenizedSending.Token;
 import sfmc.model.TokenizedSending.TokensResolveInput;
+
 import javax.json.Json;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
+import java.util.List;
 
 @Controller
 @RequestMapping("/tokens")
@@ -18,6 +21,7 @@ public class TokenizedSendingController {
     /**
      * Resolve Token
      * https://help.marketingcloud.com/en/documentation/marketing_cloud/marketing_cloud_security_and_encryption_products/tokenized_sending/creating_your_resolve_token_api___resolve_token_api_specification/
+     *
      * @return
      */
     @RequestMapping(value = "/resolve", method = RequestMethod.POST, headers = "Accept=application/json")
@@ -31,30 +35,44 @@ public class TokenizedSendingController {
     /**
      * Get Token
      * https://help.marketingcloud.com/en/documentation/marketing_cloud/marketing_cloud_security_and_encryption_products/tokenized_sending/creating_your_get_token_api___get_token_api_specification/
+     *
      * @return
      */
     @RequestMapping(value = "/get", method = RequestMethod.POST, headers = "Accept=application/json")
     public ResponseEntity getTokens(@RequestBody String input) {
+        System.out.println("*** input: " + input);
         String result = "";
         System.out.println("*** result: " + result);
         return new ResponseEntity(result, HttpStatus.OK);
     }
 
-    private String createTokens(TokensResolveInput input)
-    {
+
+    private String createTokens(TokensResolveInput input) {
         // create json
         JsonObject value = Json.createObjectBuilder()
-                .add("resolvedTokens", Json.createArrayBuilder()
-                        .add(createJsonToken("abc123thn567","user2@gmail.com"))
-                        .add(createJsonToken("abc123thn56r7","user5@gmail.com")))
+                .add("resolvedTokens", createResolvedTokens(input.getTokens()))
                 .build();
         return value.toString();
     }
 
-    private JsonObjectBuilder createJsonToken(String tokenRequestId, String tokenValue) {
-        JsonObjectBuilder value = Json.createObjectBuilder()
-                    .add("tokenRequestId", tokenRequestId)
-                    .add("tokenValue", tokenValue);
-        return value;
+
+    private JsonArrayBuilder createResolvedTokens(List<Token> tokens) {
+        javax.json.JsonArrayBuilder arr = Json.createArrayBuilder();
+        for (Token token : tokens) {
+            //
+            arr.add(Json.createObjectBuilder()
+                    .add("tokenRequestId", token.getTokenRequestId())
+                    .add("tokenValue", resolveToken(token.getToken())));
+        }
+        return arr;
+    }
+
+    private String resolveToken(String token) {
+        // TODO resolve token logic here.....
+        if(token.equals("t1234@example.com"))
+            return "user1@exacttarget.com";
+        if(token.equals("t5678@example.com"))
+            return "user2@gmail.com";
+        return token;
     }
 }
